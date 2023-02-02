8ec5e9de5083deba4f5194d8064df41274e48fa5
Wanglongzhi2001 <583087864@qq.com>
2023-02-02 21:17:43 +0800

    commit project-quiz

---
+++ .history/quiz/quiz_20230202201520.js
@@ -0,0 +1,78 @@
+const data = [
+{
+    id: 1,
+    question: "Which of these fish is actually a fish?",
+    answers: [
+    { answer: "swordfish", isCorrect: true },
+    { answer: "jellyfish", isCorrect: false },
+    { answer: "starfish", isCorrect: false },
+    { answer: "crayfish", isCorrect: false },
+    ],
+},
+{
+    id: 2,
+    question: "A flutter is a group of:",
+    answers: [
+    { answer: "bees", isCorrect: false },
+    { answer: "penguins", isCorrect: false },
+    { answer: "butterflies", isCorrect: true },
+    { answer: "camels", isCorrect: false },
+    ],
+},
+{
+    id: 1,
+    question: "A group of which animals is referred to as a wake?",
+    answers: [
+    { answer: "bats", isCorrect: false },
+    { answer: "vultures", isCorrect: true },
+    { answer: "ants", isCorrect: false },
+    ],
+},
+];
+
+
+const gameScreen = document.querySelector(".game")
+const resultScreen = document.querySelector(".result")
+const question = document.querySelector(".question")
+const answersContainer = document.querySelector(".answers")
+const submit = document.querySelector(".submit")
+const play = document.querySelector(".play")
+
+let qIndex = 0; // 问题编号
+let correctCount = 0; // 正确数目
+let wrongCount = 0; // 错误数目
+let total = 0;
+let selectedAnswer; // 选中答案的编号
+
+const showQuestion = (qNumber) =>{
+    question.textContent = data[qNumber].question;
+    answersContainer.innerHTML = data[qNumber].answers.map((item, index) =>
+    `
+    <div class="answer">
+        <input name="answer" type="radio" id=${index} value=${item.isCorrect}>
+        <label for=${index}>${item.answer}</label>
+    </div>
+    `
+    ).join("");
+
+    selectAnswer()
+};
+
+const selectAnswer = ()=>{
+    answersContainer.querySelectorAll("input").forEach(el=>{
+        el.addEventListener("click",(e)=>{
+            selectedAnswer = e.target.value
+        })
+    })
+}
+
+const submitAnswer = ()=>{
+    submit.addEventListener("click",()=>{
+        selectAnswer === "true" ? correctCount++ : wrongCount++
+        qIndex++
+        showQuestion(qIndex)
+    })
+}
+
+showQuestion(qIndex)
+submitAnswer()
---
+++ .history/quiz/quiz_20230202211433.js
@@ -0,0 +1,118 @@
+const data = [
+  {
+    id: 1,
+    question: "Which of these fish is actually a fish?",
+    answers: [
+      { answer: "swordfish", isCorrect: true },
+      { answer: "jellyfish", isCorrect: false },
+      { answer: "starfish", isCorrect: false },
+      { answer: "crayfish", isCorrect: false },
+    ],
+  },
+  {
+    id: 2,
+    question: "A flutter is a group of:",
+    answers: [
+      { answer: "bees", isCorrect: false },
+      { answer: "penguins", isCorrect: false },
+      { answer: "butterflies", isCorrect: true },
+      { answer: "camels", isCorrect: false },
+    ],
+  },
+  {
+    id: 1,
+    question: "A group of which animals is referred to as a wake?",
+    answers: [
+      { answer: "bats", isCorrect: false },
+      { answer: "vultures", isCorrect: true },
+      { answer: "ants", isCorrect: false },
+    ],
+  },
+];
+
+const gameScreen = document.querySelector(".game");
+const resultScreen = document.querySelector(".result");
+const question = document.querySelector(".question");
+const answersContainer = document.querySelector(".answers");
+const submit = document.querySelector(".submit");
+const play = document.querySelector(".play");
+
+let qIndex = 0; // 问题编号
+let correctCount = 0; // 正确数目
+let wrongCount = 0; // 错误数目
+let selectedAnswer; // 选中答案的编号
+
+// 点击playAgain
+const playAgain = () => {
+  qIndex = 0;
+  correctCount = 0;
+  wrongCount = 0;
+  showQuestion(qIndex);
+};
+
+play.addEventListener("click", () => {
+  resultScreen.style.display = "none";
+  gameScreen.style.display = "block";
+  playAgain();
+});
+
+// 结算界面
+const showResult = () => {
+  // 先将游戏界面隐藏，打开结算界面
+  resultScreen.style.display = "block";
+  gameScreen.style.display = "none";
+
+  resultScreen.querySelector(
+    ".correct"
+  ).textContent = `Correct Answers: ${correctCount}`;
+  resultScreen.querySelector(
+    ".wrong"
+  ).textContent = `Wrong Answers: ${wrongCount}`;
+  resultScreen.querySelector(".score").textContent = `Score: ${
+    (correctCount - wrongCount) * 10
+  }`;
+};
+
+// 根据问题编号在页面中生成并展示问题及选项
+const showQuestion = (qNumber) => {
+  if (qIndex === data.length) return showResult();
+  selectedAnswer = null;
+  question.textContent = data[qNumber].question;
+  answersContainer.innerHTML = data[qNumber].answers //用js生成html从而根据data数组显示对应问题
+    .map(
+      // 使用map方法遍历对象数组
+      (item, index) =>
+        `
+    <div class="answer">
+        <input name="answer" type="radio" id=${index} value=${item.isCorrect}>
+        <label for=${index}>${item.answer}</label>
+    </div>
+    `
+    )
+    .join("");
+
+  selectAnswer();
+};
+
+// 将选择的答案赋值给selectedAnswer
+const selectAnswer = () => {
+  answersContainer.querySelectorAll("input").forEach((el) => {
+    el.addEventListener("click", (e) => {
+      selectedAnswer = e.target.value;
+    });
+  });
+};
+
+// 提交答案，更新correctCount和wrongCount值
+const submitAnswer = () => {
+  submit.addEventListener("click", () => {
+    if (selectedAnswer !== null) {
+      selectedAnswer === "true" ? correctCount++ : wrongCount++;
+      qIndex++;
+      showQuestion(qIndex);
+    } else alert("Select an answer!"); // 防止不点答案就提交
+  });
+};
+
+showQuestion(qIndex);
+submitAnswer();
